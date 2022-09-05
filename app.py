ebe04a79723796d62b783da6b314a4fa9014cc6a
Grace Mugoiri <gracemugoiri@gmail.com>
2022-09-05 10:06:16 +0300

    create product

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-from distutils.log import debug
-from enum import unique
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
@@ -14,6 +12,7 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
+# Product
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True)
@@ -27,12 +26,28 @@ class Product(db.Model):
         self.price = price 
         self.qnty = qnty
 
+# ProductSchema
 class ProductSchema(ma.Schema):
     class Meta:
         fields = ('id', 'name', 'description', 'price', 'qnty')
 
+# init schema 
+product_schema = ProductSchema()
+products_schema = ProductSchema(many=True)
 
+# create a product 
+@app.route('/product', methods=["POST"])
+def add_product():
+    name = request.json["name"]
+    description = request.json["description"]
+    price = request.json["price"]
+    qnty = request.json["qnty"]
 
+    new_product = Product(name, description, price, qnty)
+    db.session.add(new_product)
+    db.session.commit()
+
+    return product_schema.jsonify(new_product)
 
 if __name__ == "__main__":
     app.run(debug=True)
